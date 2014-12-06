00253f101100b4420b89502c87a381d9c8d201b5
Clarissa Medina <clariimedina@gmail.com>
2014-12-06 13:42:54 -0700

    cambios back
    consultas

---
+++ protected/models/Ciudades.php
@@ -0,0 +1,25 @@
+<?php
+
+class Ciudades extends CActiveRecord
+{
+	public $id_ciudad;
+	public $nombre;
+
+
+	public function tableName()
+	{
+    	return 'Ciudades';
+	}
+
+	public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public function getComplejos($ciudad){
+        return Complejos::model()->getComplejosporCiudad($ciudad);
+    	
+    }
+
+
+}
